23702e17857c9c0efeefa5a60098dba04166a238
VaibhaviPatilHere <146189941+VaibhaviPatilHere@users.noreply.github.com>
2023-10-03 15:50:44 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition
+{
+public static void main(String[] args)
+ {
+	int num1=12;
+	int num2=14;
+	int sum=num1+num2;
+System.out.println("Addition of two numbers is"  +sum);
+ }
+}
